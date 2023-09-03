f140066651e928d816ccdab58d8a06b3cdf273de
hansen1416 <badapplesweetie@gmail.com>
2023-09-03 21:23:04 +0800

    1

--- rl/bouncer/src/lib.rs
+++ rl/bouncer/src/lib.rs
@@ -1,47 +1,155 @@
 use pyo3::prelude::*;
 use rapier3d::prelude::*;
-use rapier3d::na::{ Vector3 };
-use crate::nalgebra::UnitQuaternion;
+// use rapier3d::na::{ Vector3 };
 
 /// Formats the sum of two numbers as string.
-#[pyfunction]
-pub fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+// #[pyfunction]
+// pub fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+//     Ok((a + b).to_string())
+// }
+
+// pub fn create_board(position: Vector3<f32>, rotation: Vector3<f32>) {
+//     let friction: f32 = 0.0;
+//     let restitution: f32 = 1.0;
+
+//     // The set that will contain our rigid-bodies.
+//     let mut rigid_body_set = RigidBodySet::new();
+//     let mut collider_set = ColliderSet::new();
+
+//     let quaternion = UnitQuaternion::from_euler_angles(rotation.x, rotation.y, rotation.z);
+
+//     let rigid_body = RigidBodyBuilder::fixed()
+//         .translation(position)
+//         .rotation(quaternion.into())
+//         // All done, actually build the rigid-body.
+//         .build();
+//     // .rotation(rotation);
+//     let rigid_body_handle = rigid_body_set.insert(rigid_body);
+
+//     // The default density is 1.0, we are setting 2.0 for this example.
+//     let collider = ColliderBuilder::cuboid(5.0, 5.0, 0.01)
+//         .friction(friction)
+//         .restitution(restitution)
+//         .build();
+
+//     // When the collider is attached, the rigid-body's mass and angular
+//     // inertia is automatically updated to take the collider into account.
+//     let _collider_handle = collider_set.insert_with_parent(
+//         collider,
+//         rigid_body_handle,
+//         &mut rigid_body_set
+//     );
+// }
+
+// Define a struct with fields
+#[pyclass]
+pub struct Physics {
+    // A physics pipeline that executes one simulation step
+    physics_pipeline: PhysicsPipeline,
+    // A rigid body set that contains all the rigid bodies of the scene
+    rigid_body_set: RigidBodySet,
+    // A collider set that contains all the colliders of the scene
+    collider_set: ColliderSet,
+    // A ball as a rigid body handle
+    ball_body_handle: RigidBodyHandle,
 }
 
-pub fn create_board(position: Vector3<f32>, rotation: Vector3<f32>) {
-    let friction: f32 = 0.0;
-    let restitution: f32 = 1.0;
+// Implement methods for the struct
+#[pymethods]
+impl Physics {
+    // A static method that creates a new Physics
+    pub fn new() -> Physics {
+        let mut rigid_body_set = RigidBodySet::new();
+        let mut collider_set = ColliderSet::new();
 
-    // The set that will contain our rigid-bodies.
-    let mut rigid_body_set = RigidBodySet::new();
-    let mut collider_set = ColliderSet::new();
+        /* Create the ground. */
+        let collider = ColliderBuilder::cuboid(100.0, 0.1, 100.0).build();
+        collider_set.insert(collider);
 
-    let quaternion = UnitQuaternion::from_euler_angles(rotation.x, rotation.y, rotation.z);
+        /* Create the bounding ball. */
+        let rigid_body = RigidBodyBuilder::dynamic().translation(vector![0.0, 10.0, 0.0]).build();
+        let collider = ColliderBuilder::ball(0.5).restitution(0.7).build();
+        let ball_body_handle = rigid_body_set.insert(rigid_body);
+        collider_set.insert_with_parent(collider, ball_body_handle, &mut rigid_body_set);
 
-    let rigid_body = RigidBodyBuilder::fixed()
-        .translation(position)
-        .rotation(quaternion.into())
-        // All done, actually build the rigid-body.
-        .build();
-    // .rotation(rotation);
-    let rigid_body_handle = rigid_body_set.insert(rigid_body);
+        let mut physics_pipeline = PhysicsPipeline::new();
 
-    // The default density is 1.0, we are setting 2.0 for this example.
-    let collider = ColliderBuilder::cuboid(5.0, 5.0, 0.01)
-        .friction(friction)
-        .restitution(restitution)
-        .build();
+        //         self.physics_pipeline.step(
+        //     &self.gravity,
+        //     &integration_parameters,
+        //     &mut island_manager,
+        //     &mut broad_phase,
+        //     &mut narrow_phase,
+        //     &mut self.rigid_body_set,
+        //     &mut self.collider_set,
+        //     &mut self.joint_set,
+        //     None,
+        //     None,
+        //     &event_handler,
+        // );
 
-    // When the collider is attached, the rigid-body's mass and angular
-    // inertia is automatically updated to take the collider into account.
-    let _collider_handle = collider_set.insert_with_parent(
-        collider,
-        rigid_body_handle,
-        &mut rigid_body_set
-    );
+        // Return a new Physics with the fields initialized
+        Physics {
+            physics_pipeline,
+            rigid_body_set,
+            collider_set,
+            ball_body_handle,
+        }
+    }
+
+    // An instance method that updates the physics simulation and returns the ball's translation
+    pub fn update(&mut self) -> [f32; 3] {
+        /* Create other structures necessary for the simulation. */
+        let gravity = vector![0.0, -9.81, 0.0];
+        let integration_parameters = IntegrationParameters::default();
+
+        let mut island_manager = IslandManager::new();
+        let mut broad_phase = BroadPhase::new();
+        let mut narrow_phase = NarrowPhase::new();
+        let mut impulse_joint_set = ImpulseJointSet::new();
+        let mut multibody_joint_set = MultibodyJointSet::new();
+        let mut ccd_solver = CCDSolver::new();
+        let physics_hooks = ();
+        let event_handler = ();
+
+        // Execute one step of the physics simulation
+        self.physics_pipeline.step(
+            &gravity,
+            &integration_parameters,
+            &mut island_manager,
+            &mut broad_phase,
+            &mut narrow_phase,
+            &mut self.rigid_body_set,
+            &mut self.collider_set,
+            &mut impulse_joint_set,
+            &mut multibody_joint_set,
+            &mut ccd_solver,
+            None,
+            &physics_hooks,
+            &event_handler
+        );
+
+        // Get the ball's rigid body from its handle
+        let ball_rigid_body = self.rigid_body_set.get(self.ball_body_handle).unwrap();
+
+        let trans = ball_rigid_body.translation();
+
+        // Return the ball's translation (position)
+        return [trans.x, trans.y, trans.z];
+    }
 }
 
+// #[pyfunction]
+// fn bounce_ball() -> PyResult<Physics> {
+//     // Create a new Physics using the static method
+//     let mut physics = Physics::new();
+
+//     Ok(physics)
+// }
+
+/**
+ * Create a new physics struct
+ 
 #[pyfunction]
 fn run_ball() {
     let mut rigid_body_set = RigidBodySet::new();
@@ -92,11 +200,11 @@ fn run_ball() {
         println!("Ball altitude: {}", ball_body.translation().y);
     }
 }
-
+*/
 /// A Python module implemented in Rust.
 #[pymodule]
 fn bouncer(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
-    m.add_function(wrap_pyfunction!(run_ball, m)?)?;
+    // m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    // m.add_function(wrap_pyfunction!(bounce_ball, m)?)?;
     Ok(())
 }
--- rl/bouncer/src/main.rs
+++ rl/bouncer/src/main.rs
@@ -1,12 +1,19 @@
-use bouncer::create_board;
+use bouncer::Physics;
 use rapier3d::prelude::*;
 use rapier3d::na::{ Vector3 };
 
 fn main() {
-    let position: Vector3<f32> = vector![0.0, 1.0, 0.0];
-    let rotation: Vector3<f32> = vector![0.0, 0.0, 0.0];
+    // let position: Vector3<f32> = vector![0.0, 1.0, 0.0];
+    // let rotation: Vector3<f32> = vector![0.0, 0.0, 0.0];
 
-    create_board(position, rotation);
+    // create_board(position, rotation);
 
-    println!("{}", "main finished");
+    // Create a new Physics using the static method
+    let mut physics = Physics::new();
+
+    // Call the instance method to update the physics and get the ball's translation
+    let translation = physics.update();
+    println!("ball position: {:?}", translation);
+
+    // println!("{}", "main finished");
 }
